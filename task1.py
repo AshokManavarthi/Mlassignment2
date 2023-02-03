de790acad2815a2e29779cd541e9848e8851ae44
AshokManavarthi <123413689+AshokManavarthi@users.noreply.github.com>
2023-02-03 17:22:21 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 print("Program to print star pattern: \n");
-for i in range (0, 5):
-    for j in range(0, i + 1):
+for i in range (0, 5): #I loop is used for the number of columns
+    for j in range(0, i + 1): #To determine the number of stars in a column, j loop is utilized
         print("* ", end='')
-    print("\r")
+    print("\r")  # we use \r  carriage return instead of \n
 for i in range (5, 0, -1):
     for j in range(0, i -1):
         print("* ", end='')
