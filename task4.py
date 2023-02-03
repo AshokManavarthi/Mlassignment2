0050cebf18940e156069446b16fdc7b19d5915ce
AshokManavarthi <123413689+AshokManavarthi@users.noreply.github.com>
2023-02-03 17:38:12 -0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,8 +1,8 @@
-def unique_list(l):
+def unique_list(l): #used function uniquelist
   y = []
-  for a in l:
-    if a not in y:
+  for a in l: #using a for loop to traverse the given input list
+    if a not in y: #not in used to find unique list
       y.append(a)
   return y
 
-print(unique_list([1,2,3,3,3,3,4,5]))
+print(unique_list([1,2,3,3,3,3,4,5])) #function is called as given in task
