ad31594186534f2eb21293d87738e3d1daf99ccc
AshokManavarthi <123413689+AshokManavarthi@users.noreply.github.com>
2023-02-03 17:31:52 -0600

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,6 +1,6 @@
-def up_low(s):
-    u = sum(1 for i in s if i.isupper())
-    l = sum(1 for i in s if i.islower())
+def up_low(s): #Used def up_low function.
+    u = sum(1 for i in s if i.isupper()) # used for uppercase letter
+    l = sum(1 for i in s if i.islower()) #To check lower case we use islower
     print( "No. of Upper case characters : %s,No. of Lower case characters : %s" % (u,l))
 
 up_low("The quick Brow Fox")
